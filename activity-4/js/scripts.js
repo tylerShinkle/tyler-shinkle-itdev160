55e2f5df1b59f3bc03a2368055b9f5282af921ad
unknown <tyshinkle@gmail.com>
2019-03-09 23:22:52 -0600

    Completed prices and discounted prices for each product.

--- activity-4/js/scripts.js
+++ activity-4/js/scripts.js
@@ -4,3 +4,40 @@ var greeting=salutation+" "+user;
 
 var greetingElement=document.getElementById('greeting');
 greetingElement.textContent=greeting;
+
+//welcome completed.
+
+//studentDiscount never changes.
+const studentDiscount=.10;
+
+var iPhonePrice=200;
+var iPhoneStudentPrice=iPhonePrice-(iPhonePrice*studentDiscount);
+
+var iPhonePriceElement=document.getElementById('iPhonePrice');
+var iPhoneStudentPriceElement=document.getElementById('iPhoneStudentPrice');
+
+iPhonePriceElement.textContent=" $"+iPhonePrice;
+iPhoneStudentPriceElement.textContent=" $"+iPhoneStudentPrice;
+
+//iphone done.
+
+var carPrice=4000;
+var carStudentPrice=carPrice-(carPrice*studentDiscount);
+
+var carPriceElement=document.getElementById('carPrice');
+var carStudentPriceElement=document.getElementById('carStudentPrice');
+
+carPriceElement.textContent=" $"+carPrice;
+carStudentPriceElement.textContent=" $"+carStudentPrice;
+
+//car done
+var bookPrice=30;
+var bookStudentPrice=bookPrice-(bookPrice*studentDiscount);
+
+var bookPriceElement=document.getElementById('bookPrice');
+var bookStudentPriceElement=document.getElementById('bookStudentPrice');
+
+bookPriceElement.textContent=" $"+bookPrice;
+bookStudentPriceElement.textContent=" $"+bookStudentPrice;
+
+//book done
