bf406a4731625c4026086b41c649659ca83551a9
Tyler Shinkle <tyshinkle@gmail.com>
2019-04-06 21:53:11 -0500

    Worked on overlay, added exit button functionality and header, learned about element values for tying buttons and such to arrays ... do reply button and other stuff tomorrow

--- activity-6/js/scripts.js
+++ activity-6/js/scripts.js
@@ -1,23 +1,23 @@
 //add eventListener to start button.
 var start = document.getElementById('startButton');
-start.addEventListener('click',init,false);
+start.addEventListener('click', init, false);
 
 //message object constructor...
 //add type to differentiate from reply and send...
-function Message(to, from, subject, msg,type) {
+function Message(to, from, subject, msg, type) {
   this.to = to;
   this.from = from;
   this.subject = subject;
   this.msg = msg;
   //s for send, r for reply and i for in...
-  this.type=type;
+  this.type = type;
 }
 
 
 //creating initial messages and loading them into message array.
-msg1 = new Message("Tyler", "Bill", "Homework", "Hello, I was wondering if you knew what homework was assigned, I missed class. Also, we're going to test overflow. Blah Blah Blah Blah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah Blahv Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah BlahBlah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah BlahBlah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah BlahBlah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah","i");
-msg2 = new Message("Tyler", "Jill", "Recipe", "Hi, I was wondering if you could send me that brownie recipe? They were excellent. Thanks!","i");
-msg3 = new Message("Tyler", "Luisa", "Whaddup", "Hey, I was wondering if you wanted to hang after work. Let me know.","i");
+msg1 = new Message("Tyler", "Bill", "Homework", "Hello, I was wondering if you knew what homework was assigned, I missed class. Also, we're going to test overflow. Blah Blah Blah Blah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah Blahv Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah BlahBlah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah BlahBlah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah BlahBlah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah", "i");
+msg2 = new Message("Tyler", "Jill", "Recipe", "Hi, I was wondering if you could send me that brownie recipe? They were excellent. Thanks!", "i");
+msg3 = new Message("Tyler", "Luisa", "Whaddup", "Hey, I was wondering if you wanted to hang after work. Let me know.", "i");
 var messages = [msg1, msg2, msg3];
 
 
@@ -62,7 +62,7 @@ function init() {
     //line break before message.
     msgBlock.appendChild(document.createElement("br"));
     //add div for message and append msg text.
-    var msgArea = document.createElement("div");
+    var msgArea = document.createElement("p");
     msgArea.classList.add("msgArea");
     var msgText = document.createTextNode(messages[i].msg);
     msgArea.appendChild(msgText);
@@ -72,21 +72,50 @@ function init() {
     replyContainer.classList.add("replyContainer");
     var replyButton = document.createElement("button");
     replyButton.classList.add("replyButton");
-    var replyText = document.createTextNode("Reply");
+    //YES!!
+    replyButton.value = i;
+    //Yes!!
+    var replyText = document.createTextNode(i + "Reply");
     replyButton.appendChild(replyText);
-    // add eventListener here... this will call the addMessageHandler...
-    replyButton.addEventListener('click',addMessageHandler,false);
     replyContainer.appendChild(replyButton);
     //put button in div
     msgBlock.appendChild(replyContainer);
-
-
+    // add eventListener here... this will call the addMessageHandler...
+    replyButton.addEventListener('click', function() {
+      addMessageHandler('r', this.value);
+    }, false);
   }
 }
 
 //addMessageHandler...
 
-function addMessageHandler(){
-  msgWindow=document.getElementById('interActContainer');
-  msgWindow.style.display="block";
+function addMessageHandler(type, i) {
+  //shorthand for buttons
+  sendButton = document.getElementById('send-button');
+  replyButton = document.getElementById('reply-button');
+  messageInput = document.getElementById('message-input');
+  msgWindow = document.getElementById('interActContainer');
+  //load header text...
+  to = document.getElementById('toForCreate');
+  toText = document.createTextNode("To: " + messages[i].to);
+  to.appendChild(toText);
+  from = document.getElementById('fromForCreate');
+  fromText = document.createTextNode("From: " + messages[i].from);
+  from.appendChild(fromText);
+  subject = document.getElementById('subjectForCreate');
+  subjectText = document.createTextNode("Subject: " + messages[i].subject);
+  subject.appendChild(subjectText);
+  //assign functions to buttons...
+  exitButton = document.getElementById('exit-button');
+  exitButton.addEventListener('click', function() {
+    msgWindow.style.display = "none";
+    toText.nodeValue = "";
+    fromText.nodeValue = "";
+    subjectText.nodeValue = "";
+  }, false);
+  if (type = "r") {
+    msgWindow.style.display = "block";
+    sendButton.style.display = "none";
+
+  }
 }
